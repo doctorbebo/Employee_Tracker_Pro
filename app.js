4f74b9d9b0ff81469501b409fd4d5e5248676a72
doctorbebo <byronrb92@gmail.com>
2020-02-22 16:46:21 -0800

    updatess

--- app.js
+++ app.js
@@ -4,12 +4,21 @@ const Intern = require("./lib/Intern");
 const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
-​
+
 const OUTPUT_DIR = path.resolve(__dirname, "output")
 const outputPath = path.join(OUTPUT_DIR, "team.html");
-​
+
 const render = require("./lib/htmlRenderer");
-​
+
+const m = new Manager("Bob", 232, "Bob@test.com", 100);
+console.log(m);
+
+
+
+
+
+
+
 ​
 // Write code to use inquirer to gather information about the development team members,
 // and to create objects for each team member (using the correct classes as blueprints!)
