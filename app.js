95600790dc07ad8989bce5cc1d37a180ae3e62be
doctorbebo <byronrb92@gmail.com>
2020-02-23 12:58:29 -0800

    Update app.js

--- app.js
+++ app.js
@@ -10,7 +10,98 @@ const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
 
+const employeeArray =[];
 
+const firstQuestions =[
+{
+    type: "list",
+    name : "role",
+    message : "What postion is the employee?",
+    choices : ["Intern", "Manager", "Engineer"]
+},
+{
+    type : "input",
+    name :  "name",
+    message : "What is the employee's name?"
+},
+{
+    type : "input",
+    name :  "id",
+    message : "What is the employee's id?"
+},
+{
+    type : "input",
+    name :  "email",
+    message : "What is the employee's email?"
+}]
+const internQuestions =
+{
+    type : "input",
+    name : "school",
+    message : "What school is the intern attending?"
+}
+const managerQuestions = 
+{
+    type : "input",
+    name : "officeNumber",
+    message : "What is the manager's office numbner?"
+}
+const engineerQuestions =
+{
+    type : "input",
+    name : "gitHubUsername",
+    message : "What is the engineer's GitHub username?"
+}
+function employeeQuestions()
+{
+    inquirer.prompt(firstQuestions).then(answers =>
+    {
+        switch (answers.role) {
+            case "Intern":
+                inquirer.prompt(internQuestions).then(internAnswers =>
+                    {
+                        let e = new Intern(answers.name, answers.id, answers.email, internAnswers.school);
+                        employeeArray.push(e)
+                        console.log(employeeArray); 
+                        anotherEmployee()      
+                    })
+                break;
+            case "Manager":
+                inquirer.prompt(managerQuestions).then(managerAnswers =>
+                    {
+                        let e = new  Manager(answers.name, answers.id, answers.email, managerAnswers.officeNumber);
+                        employeeArray.push(e)
+                        console.log(employeeArray);
+                        anotherEmployee()        
+                    })
+                break;
+            case "Engineer":
+                inquirer.prompt(engineerQuestions).then(engineerAnswers =>
+                    {
+                        let e = new Engineer(answers.name, answers.id, answers.email, engineerAnswers.gitHubUsername);
+                        employeeArray.push(e)
+                        console.log(employeeArray);
+                        anotherEmployee()
+                    })
+                break;
+        }
+    });
+}
+function anotherEmployee()
+{
+    inquirer.prompt({type: "confirm", name : "another", message : "Do you want to add another Employee?"})
+    .then(another => {
+        if (another.another)
+        {
+            employeeQuestions();   
+        }else
+        {
+            console.log(render(employeeArray));
+        }
+    })
+}
+
+employeeQuestions();
 // Write code to use inquirer to gather information about the development team members,
 // and to create objects for each team member (using the correct classes as blueprints!)
 
